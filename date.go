3ec6fa25387b2d7cbaf1517f53c75d946ff0a3ab
Clint Armstrong <clint@clintarmstrong.net>
2016-07-19 11:54:03 -0400

    prevent 1 second delay on starting datetime

--- date.go
+++ date.go
@@ -4,12 +4,13 @@ import "time"
 
 func DateTime(uc chan<- []I3Block) {
 	b := make([]I3Block, 3)
+	t := time.Now()
 	tc := time.Tick(1 * time.Second)
 	for {
-		t := <-tc
 		b[0].FullText = t.UTC().Format("2006-01-02")
 		b[1].FullText = t.UTC().Format("15:04:05 MST")
 		b[2].FullText = t.Local().Format("15:04:05 MST")
 		uc <- b
+		t = <-tc
 	}
 }
